74dceabb6ee82ca1140124a0e02e987d6934d1c6
Benjamin Legrand <jibhaine@free.fr>
2018-12-06 08:50:32 +0100

    fix ci

--- app/message/schema/HeroMessage.ts
+++ app/message/schema/HeroMessage.ts
@@ -7,16 +7,14 @@ export enum HeroMessagesTypes {
   Quit,
   Action,
 }
-var HeroSchema = schemapack.build({
-  health: "varuint",
-  jumping: "boolean",
-  position: ["int16"],
-  attributes: { str: "uint8", agi: "uint8", int: "uint8" },
-});
 
-export class HeroMessage extends BehaviorSubject<HeroSchema>
-  implements Message<HeroMessagesTypes, HeroSchema> {
-  create(type: HeroMessagesTypes, schema: HeroSchema) {
-    super();
+export class HeroMessage {
+  create(type: HeroMessagesTypes) {
+    return schemapack.build({
+      health: "varuint",
+      jumping: "boolean",
+      position: ["int16"],
+      attributes: { str: "uint8", agi: "uint8", int: "uint8" },
+    });
   }
 }
