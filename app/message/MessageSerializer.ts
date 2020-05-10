220a1f9c842d83bfdfba64c69821fd853a2f2fc6
Benjamin Legrand <benjilegnard@protonmail.com>
2020-05-10 19:00:55 +0200

    wip

--- app/message/MessageSerializer.ts
+++ app/message/MessageSerializer.ts
@@ -1,4 +1,13 @@
-export interface Serializer<T> {
+export abstract class Serializer<T> {
   encodeSchema?: (input: T) => Buffer;
   decodeSchema?: (buffer: Buffer | object) => T;
 }
+
+export abstract class AbstractSerializer<T> implements Serializer<T> {
+  encodeSchema: (input: T) => Buffer = () => {
+    return new Buffer("");
+  };
+  decodeSchema: (buffer: Buffer | object) => T = () => {
+    return {} as T;
+  };
+
---
+++ app/server/services/effects.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "injection-js";
+import { Observable } from "rxjs";
+import { switchMap, filter } from "rxjs/operators";
+import { Action } from "../../actions";
+
+@Injectable()
+export class ServerEffects {
+  connectionEffect$ = this.actions$.pipe(
+    //filter(action => action.type === "Connect"),
+  );
+
+  constructor(private actions$: Observable<Action>) {}
+}
