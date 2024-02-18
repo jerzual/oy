8cd29699d5a47792ae45a74062c51221ae4bab86
Benjamin Legrand <benjilegnard@protonmail.com>
2024-02-18 18:54:43 +0100

    upgrade all the things

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/OY/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Play' }).click();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // Expects the URL to contain join.
+  await expect(page).toHaveURL(/.*join/);
 });
